a7672d371faaba9a740661738e04064c75d56ed6
andresalarcon <w.alarconc@uniandes.edu.co>
2023-11-02 19:02:06 -0500

    Se crea el modulo, el servicio, la clase y el componente para mostrar las plantas

---
+++ parcial-plantas/src/app/plantas/planta-list/planta-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planta-list',
+  templateUrl: './planta-list.component.html',
+  styleUrls: ['./planta-list.component.css']
+})
+export class PlantaListComponent {
+
+}
---
+++ parcial-plantas/src/app/plantas/planta.ts
@@ -0,0 +1,20 @@
+export class Planta{
+
+  id: number;
+  nombre_comun: string;
+  nombre_cientifico: string;
+  tipo: string;
+  altura_maxima: number;
+  clima: string;
+  sustrato_siembra: string;
+  constructor(id: number, nombre_comun: string, nombre_cientifico: string, tipo: string, altura_maxima: number,
+              clima: string, sustrato_siembra: string) {
+    this.id = id;
+    this.nombre_comun = nombre_comun;
+    this.nombre_cientifico = nombre_cientifico;
+    this.tipo = tipo;
+    this.altura_maxima = altura_maxima;
+    this.clima = clima;
+    this.sustrato_siembra = sustrato_siembra;
+  }
+}
